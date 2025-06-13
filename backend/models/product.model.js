606a7194b6bef57f5e8e1fa6f5faa025bc25e110
Mark Knight <markknight@Marks-MacBook-Air.local>
2025-06-13 15:41:04 +0100

    Added API's - GET/POST/DELETE/PUT

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  image: { type: String, required: true },
-  timestamp: true,
-});
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+  },
+  { timestamps: true } // <-- Correct way to enable timestamps
+);
 
 const Product = mongoose.model("Product", productSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import productRoutes from './routes/product.route.js';
+
 dotenv.config();
 
 const app = express();
 
-app.get('/products', (req, res) => {});
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
 
 console.log(process.env.MONGO_URI);
 
